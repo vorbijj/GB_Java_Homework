7d580d3d881a34df6dcdd6d410c54b71620fee9f
Ilya Vorobyev <vorbinn@list.ru>
2021-04-15 02:19:47 +0300

    Выполнено ДЗ №7

--- src/com/company/lesson7/Plate.java
+++ src/com/company/lesson7/Plate.java
@@ -2,13 +2,29 @@ package com.company.lesson7;
 
 public class Plate {
     private int food;
+    private boolean decreased;
 
     public Plate(int food) {
         this.food = food;
+        decreased = false;
+    }
+
+    public void setFood(int food) {
+        this.food += food;
+    }
+
+    public boolean isDecreased() {
+        return decreased;
     }
 
     public void decreaseFood(int n) {
-        food -= n;
+        if ((food - n) >= 0) {
+            food -= n;
+            decreased = true;
+        } else {
+            System.out.println("Недостаточно еды в миске");
+            decreased = false;
+        }
     }
 
     public void info() {
