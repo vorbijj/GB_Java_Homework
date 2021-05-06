4a1fee12916711739ed4b40da35e2e2b6ab9e33c
Ilya Vorobyev <vorbinn@list.ru>
2021-05-06 18:19:24 +0300

    Сделано ограничение для тоннеля, половина машин. ДЗ №13

--- src/com/company/lesson13/Tunnel.java
+++ src/com/company/lesson13/Tunnel.java
@@ -1,6 +1,10 @@
 package com.company.lesson13;
 
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage {
+    Semaphore smp = new Semaphore(MainClass.CARS_COUNT/2);
+
     public Tunnel() {
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
@@ -11,12 +15,14 @@ public class Tunnel extends Stage {
         try {
             try {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                smp.acquire();
                 System.out.println(c.getName() + " начал этап: " + description);
                 Thread.sleep(length / c.getSpeed() * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
                 System.out.println(c.getName() + " закончил этап: " + description);
+                smp.release();
             }
         } catch (Exception e) {
             e.printStackTrace();
